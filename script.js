0c8c4fd1aec55ea82ef89e27eafabbdb2f2d5f8e
RaphaelalexisBiver <71467424+RaphaelalexisBiver@users.noreply.github.com>
2020-12-04 19:17:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = prompt(`Digite seu nome`);
+
+function bemvinda(){
+alert (`Bem vindo ${nome}, nosso site ainda está no beta, aproveite o conhecimento sobre os nossos atendimentos obg ;-;.`)
+
+}
+
+window.onload = bemvinda;
